c7ebd9cda5e44a273cfc48f5a5880c10bfcfaef8
Mostafa-Essam-Bakr <elturkeym0@gmail.com>
2023-12-29 19:23:30 +0200

    first commit1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import im1 from "./im1.jpg"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <img src={im1}  alt="logo" />
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
